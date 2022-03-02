d71447170c0800fc2c6dcb108ccbdb588e64f418
Adrian Barreto Arenaz <abarretoa@unav.es>
2022-03-02 10:09:13 +0100

    useEffect

---
+++ src/components/ResizeApp.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+
+const ResizeApp = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const isMobile = windowWidth < 768;
+
+  useEffect(() => {
+      const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+  
+    return () =>{
+        console.log('return useeffect');
+        window.removeEventListener("resize", handleResize);
+    }
+}, []);
+
+  return (
+    <div>
+      <h1>{windowWidth}</h1>
+      {isMobile && 
+        <h2>Show only mobile device</h2>
+      }
+      
+    </div>
+  );
+};
+
+export default ResizeApp;
