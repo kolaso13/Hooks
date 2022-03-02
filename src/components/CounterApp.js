bde8d9f5945a99c82ae0470c0d9db1cd1b1a9dc0
Adrian Barreto Arenaz <abarretoa@unav.es>
2022-03-02 12:42:06 +0100

    useReducer

---
+++ src/components/CounterApp.js
@@ -0,0 +1,44 @@
+import { useReducer } from "react";
+
+const types = {
+  increment: "increment",
+  decrement: "decrement",
+  reset: "reset",
+};
+
+const initialState = 0.23232323;
+
+const init = (value) => {
+  return parseInt(value);
+}
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.increment:
+      return state + 1;
+    case types.decrement:
+      return state - 1;
+    case types.reset:
+      return init(initialState);
+    default:
+      return state;
+  }
+};
+
+const CounterApp = () => {
+  const [counter, dispatch] = useReducer(reducer, initialState, init);
+
+  return (
+    <div>
+      <h1>Clicks: {counter}</h1>
+      <button onClick={() => dispatch({ type: types.increment })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: types.decrement })}>
+        Decrement
+      </button>
+      <button onClick={() => dispatch({ type: types.reset })}>Reset</button>
+    </div>
+  );
+};
+
+export default CounterApp;
---
+++ src/components/ProductApp.js
@@ -0,0 +1,67 @@
+import { useReducer } from "react";
+import productReducer, {
+  initialProductState,
+} from "../reducers/productReducer";
+import types from "./type";
+const ProductApp = () => {
+  const [productState, dispatch] = useReducer(
+    productReducer,
+    initialProductState
+  );
+  const { products, cart, activeProduct } = productState;
+
+  return (
+    <div>
+      <h2>Products</h2>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.title}
+            <button
+              onClick={() =>
+                dispatch({
+                  type: types.productShow,
+                  payload: product,
+                })
+              }
+            >
+              Show
+            </button>
+            <button
+              onClick={() =>
+                dispatch({
+                  type: types.productAddToCart,
+                  payload: product,
+                })
+              }
+            >
+              Add to cart
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Cart</h2>
+      <ul>
+        {cart.map((product) => (
+          <li key={product.id}>
+            {product.title} - quantity: {product.quantity}
+            <button onClick={() => dispatch({
+                type: types.productRemoveOneFromCart,
+                payload: product.id
+            })}>Remove One</button>
+
+            <button onClick={() => dispatch({
+                type: types.productRemoveFromCart,
+                payload: product.id
+            })}>Remove from cart</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Preview</h2>
+      <p>{activeProduct.title}</p>
+    </div>
+  );
+};
+export default ProductApp;
